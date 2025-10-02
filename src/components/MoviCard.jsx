a1e2c7bed0a2a6eb067303967d69787d8348db18
milantimalsena <milantimalsena87@gmail.com>
2025-10-02 22:51:05 +0545

    added button

--- src/components/MoviCard.jsx
+++ src/components/MoviCard.jsx
@@ -1,25 +1,30 @@
 import Movies from "./Movies"
+import { useState } from "react"
 
 function MovieCard({ movie }) {
+    const [searchQuery, setSearchQuery] = useState("");
 const movies = [
     {Title : "Milan", Year : "2024"},
     {Title : "Pathaan", Year : "2023"},
     {Title : "Tiger", Year : "2022"},
     {Title : "War", Year : "2021"},
     {Title : "Dhoom", Year : "2020"}
+
 ];
 
 const handleSearch = (event) => {
-    event.preventDefault();
-   
-
-}
-   
+  event.preventDefault();
+  alert(`Searching for ${searchQuery}`);
+  setSearchQuery("");
+};
 
     return(
         <div className="movie-container">
             <form onSubmit={handleSearch}className="search-form">
-                <input type="text" placeholder="Search movies..." className="search-input" />
+
+                <input type="text" placeholder="Search movies..." className="search-input" value={searchQuery}
+                onChange={(e) => setSearchQuery(e.target.value)} />
+
                 <button type="submit" className="search-button">Search</button>
 
                 </form>
